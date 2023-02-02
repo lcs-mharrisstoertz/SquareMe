613c10362760d2d7fc50250acccf1107b33897c3
Morgan <Mharrisstoertz@gmail.com>
2023-02-02 14:05:19 -0500

    Delted content view

---
+++ SquareMe/RootsView.swift
@@ -0,0 +1,20 @@
+//
+//  RootsView.swift
+//  SquareMe
+//
+//  Created by Morgan Harris-Stoertz on 2023-02-02.
+//
+
+import SwiftUI
+
+struct RootsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RootsView_Previews: PreviewProvider {
+    static var previews: some View {
+        RootsView()
+    }
+}
