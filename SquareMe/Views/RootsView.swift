7d2073504b37b85905271b84e79136f7ccd483aa
Morgan <Mharrisstoertz@gmail.com>
2023-02-02 14:22:45 -0500

    written a computed property to get the input as a double

--- SquareMe/Views/RootsView.swift
+++ SquareMe/Views/RootsView.swift
@@ -15,12 +15,24 @@ struct RootsView: View {
     @State var inputGiven = ""
     
     //MARK: computed properties
+    //convert input given into an optional double
+    var inputGivenAsOptionalDouble: Double?{
+        
+        //attempt to create the optional Double
+        guard let inputGivenAsDouble = Double(inputGiven) else {
+            //user gave bad input and we cannot convert to a double
+            return nil
+        }
+        //we have an actual double so return it
+        return inputGivenAsDouble
+    }
+    
 
     
     var body: some View {
         NavigationView{
             VStack{
-                TextField("5.5", text: $inputGiven)
+                TextField("Enter a number...", text: $inputGiven)
                     .font(.largeTitle)
                     .padding()
                 
